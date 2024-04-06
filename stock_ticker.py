b3d2dbec3b45bacca1a892403005a75d346e698f
Bryce Erickson <erickson.br@northeastern.edu>
2024-04-06 10:33:20 -0400

    updating 60 to 59

--- stock_ticker.py
+++ stock_ticker.py
@@ -41,7 +41,7 @@ def parse_arguments():
     """Parse command line arguments for stock tickers."""
     parser = argparse.ArgumentParser(description='Stock Ticker Display')
     parser.add_argument('ticker', nargs=2, help='Two stock tickers')
-    parser.add_argument('--refresh', type=check_positive, default=60, help='Refresh rate in seconds (default: 60, min: 1)')
+    parser.add_argument('--refresh', type=check_positive, default=59, help='Refresh rate in seconds (default: 59, min: 1)')
     args = parser.parse_args()
     return args
 
